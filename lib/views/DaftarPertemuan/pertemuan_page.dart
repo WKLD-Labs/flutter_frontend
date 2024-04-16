9bd8ce91e1bc55a2c522393c47ce6d518e947335
ecospheree <121998521+ecospheree@users.noreply.github.com>
2024-04-16 08:08:08 +0700

    DaftarPertemuan semi-final
    seharusnya aman

--- lib/views/DaftarPertemuan/pertemuan_page.dart
+++ lib/views/DaftarPertemuan/pertemuan_page.dart
@@ -14,16 +14,105 @@ class DaftarPertemuan extends StatefulWidget {
 
 class _DaftarPertemuanState extends State<DaftarPertemuan> {
   bool isDialogVisible = false;
-  List<Map<String, dynamic>> meetingData = [];
+  List<Map<String, dynamic>> meetingData = [
+    
+    {
+      'meetingname':'Konsultasi TA',
+      'speaker':'dr. Rani Saprina',
+      'date':'2024-04-16',
+      'time':'10:50 AM',
+      'meetinglink': 'https://york-ac-uk.zoom.us/j/96738689931',
+      'description': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+
+    {
+      'meetingname':'Konsultasi TA',
+      'speaker':'dr. Richard Lee',
+      'date':'2024-5-20',
+      'time':'11:00 AM',
+      'meetinglink': 'https://york-ac-uk.zoom.us/j/96738689931',
+      'description': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    },
+
+    {
+      'meetingname':'Konsultasi TA',
+      'speaker':'dr. Suzy Maria',
+      'date':'2024-04-12',
+      'time':'9:50 AM',
+      'meetinglink': 'https://york-ac-uk.zoom.us/j/96738689931',
+      'description': '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    }
+
+  ];
   Map<String, dynamic>? selectedData;
   bool showView = false;
+  int? selectedIndex;
 
-  void updateMeetingData() {
-    // Implement your logic to fetch meeting data here
+  void updateMeetingData() {}
+
+  void handleCreate(Map<String, dynamic> data) {}
+
+  void deleteMeeting(int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Hapus "Data pertemuan"?'),
+          content:
+              Text('Apakah anda yakin ingin menghapus data pertemuan ini?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Batal'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  meetingData.removeAt(index);
+                });
+                Navigator.of(context).pop();
+              },
+              child: Text('Hapus'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
-  void handleCreate(Map<String, dynamic> data) {
-    // Implement your logic to handle create operation here
+  TextEditingController _dateController = TextEditingController();
+  TextEditingController _timeController = TextEditingController();
+  
+  Future<void> _selectTime(BuildContext context) async {
+  final TimeOfDay? pickedTime = await showTimePicker(
+    context: context,
+    initialTime: TimeOfDay.now(),
+  );
+  if (pickedTime != null) {
+    final String formattedTime = pickedTime.format(context);
+    setState(() {
+      _timeController.text = formattedTime;
+    });
+    _handleChange('time', formattedTime);
+  }
+}
+
+
+  Future<void> _selectDate() async {
+    DateTime? _picked = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100));
+
+    if (_picked != null) {
+      setState(() {
+        _dateController.text = _picked.toString().split(" ")[0];
+        _handleChange('date', _dateController.text);
+      });
+    }
   }
 
   @override
@@ -40,21 +129,13 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 8),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      isDialogVisible = true;
-                      selectedData = {}; // Initialize selectedData
-                    });
-                  },
-                  child: Text('New Meeting'),
-                ),
-                SizedBox(height: 16),
-                ListView.builder(
+                ListView.separated(
                   shrinkWrap: true,
                   itemCount: meetingData.length,
-                  itemBuilder: (context, index) {
+                  separatorBuilder: (BuildContext context, int index) {
+                    return SizedBox(height: 8);
+                  },
+                  itemBuilder: (BuildContext context, int index) {
                     return GestureDetector(
                       onTap: () {
                         setState(() {
@@ -63,14 +144,13 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
                         });
                       },
                       child: Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        padding: EdgeInsets.all(8),
+                        padding: EdgeInsets.all(12),
                         decoration: BoxDecoration(
-                          border: Border.all(color: Colors.grey),
+                          color: Colors.grey[200],
                           borderRadius: BorderRadius.circular(8),
                         ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Text(
                               meetingData[index]['meetingname'],
@@ -79,18 +159,20 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
                                 fontSize: 16,
                               ),
                             ),
-                            SizedBox(height: 4),
-                            Text(
-                              'Speaker: ${meetingData[index]['speaker']}',
-                            ),
-                            Text(
-                              'Date & Time: ${meetingData[index]['datetime']}',
-                            ),
-                            Text(
-                              'Meeting Link: ${meetingData[index]['meetinglink']}',
-                            ),
-                            Text(
-                              'Description: ${meetingData[index]['description']}',
+                            PopupMenuButton<String>(
+                              onSelected: (String choice) {
+                                if (choice == 'hapus') {
+                                  deleteMeeting(index);
+                                }
+                              },
+                              itemBuilder: (BuildContext context) {
+                                return ['hapus'].map((String choice) {
+                                  return PopupMenuItem<String>(
+                                    value: choice,
+                                    child: Text(choice),
+                                  );
+                                }).toList();
+                              },
                             ),
                           ],
                         ),
@@ -112,25 +194,44 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
                     children: [
                       TextFormField(
                         decoration: InputDecoration(labelText: 'Meeting Name'),
-                        onChanged: (value) => _handleChange('meetingname', value),
+                        onChanged: (value) =>
+                            _handleChange('meetingname', value),
                       ),
                       TextFormField(
                         decoration: InputDecoration(labelText: 'Speaker'),
                         onChanged: (value) => _handleChange('speaker', value),
                       ),
                       TextFormField(
-                        decoration: InputDecoration(labelText: 'Date & Time'),
-                        onChanged: (value) => _handleChange('Date & Time', value),
+                          controller: _dateController,
+                          onTap: () {
+                            _selectDate();
+                          },
+                          readOnly: true,
+                          decoration: InputDecoration(labelText: 'Date'),
+                          onChanged: (value) {
+                            if (value != null) {
+                              _handleChange('date', _dateController.text);
+                            }
+                          }),
+                      TextFormField(
+                        controller: _timeController,
+                        decoration: InputDecoration(labelText: 'Time'),
+                        onChanged: (value) => _handleChange('time', value),
+                        onTap: (){
+                          _selectTime(context);
+                        },
+                        readOnly: true,
                       ),
                       TextFormField(
                         decoration: InputDecoration(labelText: 'Meeting Link'),
-                        onChanged: (value) => _handleChange('Meeting Link', value),
+                        onChanged: (value) =>
+                            _handleChange('meetinglink', value),
                       ),
                       TextFormField(
                         decoration: InputDecoration(labelText: 'Description'),
-                        onChanged: (value) => _handleChange('Description', value),
+                        onChanged: (value) =>
+                            _handleChange('description', value),
                       ),
-                      // Add more form fields for other meeting details
                     ],
                   ),
                 ),
@@ -153,14 +254,43 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
           if (showView)
             BackdropFilter(
               filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-              child: ViewPertemuanData(
-                showDialog: showView,
-                setShowDialog: (value) => setState(() => showView = value),
-                data: selectedData!,
+              child: AlertDialog(
+                title: Text(selectedData!['meetingname']),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: <Widget>[
+                      Text("Speaker: ${selectedData!['speaker']}"),
+                      Text("Date: ${selectedData!['date']}"),
+                      Text("Time: ${selectedData!['time']}"),
+                      Text("Meeting Link: ${selectedData!['meetinglink']}"),
+                      Text("Description: ${selectedData!['description']}"),
+                    ],
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        showView = false;
+                      });
+                    },
+                    child: Text('Close'),
+                  ),
+                ],
               ),
             ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              isDialogVisible = true;
+              selectedData = {};
+            });
+          },
+          child: Icon(Icons.add),
+          shape: CircleBorder(),
+          backgroundColor: Colors.white),
       endDrawer: NavDrawer(context),
     );
   }
@@ -174,69 +304,42 @@ class _DaftarPertemuanState extends State<DaftarPertemuan> {
   }
 
   void _handleSubmit() {
-    handleCreate(selectedData!);
-    setState(() {
-      isDialogVisible = false;
-      if (selectedData != null) {
-        meetingData.add(selectedData!); // Add the new meeting to the list
-      }
-    });
+    if (selectedData?.length == 6) {
+      handleCreate(selectedData!);
+      setState(() {
+        isDialogVisible = false;
+        if (selectedData != null) {
+          meetingData.add(Map<String, dynamic>.from(selectedData!));
+        }
+        selectedData = null;
+        _dateController.clear();
+        _timeController.clear();
+      });
+    } else {
+      showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Text('Please complete all fields before saving.'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   void _handleClose() {
     setState(() {
       isDialogVisible = false;
-      selectedData = null; // Reset selectedData when closing the dialog
+      selectedData = null;
     });
-  }
-}
-
-class ViewPertemuanData extends StatelessWidget {
-  final bool showDialog;
-  final Function(bool) setShowDialog;
-  final Map<String, dynamic> data;
-
-  const ViewPertemuanData({
-    Key? key,
-    required this.showDialog,
-    required this.setShowDialog,
-    required this.data,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              data['meetingname'],
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 8),
-            Text(data['speaker'] ?? ''), // Add null check for speaker
-            Text(data['datetime'] != null ? data['datetime'] : ''), // Check if datetime is not null
-            Text(data['meetinglink'] ?? ''), // Add null check for meetinglink
-            Text(data['description'] ?? ''), // Add null check for description
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                OutlinedButton(
-                  onPressed: () => setShowDialog(false),
-                  child: Text('Close'),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
